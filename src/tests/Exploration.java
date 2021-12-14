402858354f36f4e0c84124d7af623289366fb0d1
Pierre Anken <pierre.anken@pm.me>
2021-12-14 23:06:48 +0100

    gitignore + comment on graph

--- src/tests/Exploration.java
+++ src/tests/Exploration.java
@@ -6,14 +6,16 @@ public class Exploration {
 
 	public static void main(String[] args) {
 		
-		int matrice[][] = {{1, 2, 3},
-				{4},
+		int matrice[][] = {
+				{1, 2, 3}, 	// Le noeud 0 à un lien vers 1-2-3
+				{4},		// Le noeud 1 à un lien vers 4
 				{4,5,6},
 				{6},
 				{7},
 				{7},
 				{7},
-				{}};
+				{}
+		};
 
 			
 		Graphe graphe = new Graphe(matrice);
